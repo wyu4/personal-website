3b9b5bcdf53cd2217f38f8060a971cf6fd1334a8
wyu4 <wilsonyu657@gmail.com>
2026-03-30 00:51:58 -0400

    Add working repository API

--- backend/src/api/repository.ts
+++ backend/src/api/repository.ts
@@ -1,7 +1,67 @@
 import { Express } from "express";
+import {
+    createSupabase,
+    getTable,
+    Owner,
+    SimplifiedRepository,
+    Table,
+} from "../helpers/external";
 
 const { GITHUB_API_KEY } = require("./../helpers/environment");
 
+type Repository = SimplifiedRepository & {
+    owner: Owner;
+};
+
+let allRepositories: SimplifiedRepository[] | undefined = undefined;
+let publicRepositories: SimplifiedRepository[] | undefined = undefined;
+let owners: Owner[] = [];
+
 export const createRepositoriesAPI = (app: Express) => {
-    
+    const supabase = createSupabase();
+
+    const setRepositories = (data: SimplifiedRepository[] | null) => {
+        if (!data) return;
+        allRepositories = data;
+        publicRepositories = data.filter(
+            (repo) => repo.visibility === "public",
+        );
+
+        console.log(
+            `📃 Updated repositories. ${publicRepositories.length}/${allRepositories.length} repositories will be exposed.`,
+        );
+    };
+
+    app.get("/api/repositories", async (req, res) => {
+        console.log(`<<< Received [/api/repositories] ping from ${req.ip}.`);
+
+        if (supabase) {
+            await getTable("github_repository", setRepositories);
+            await getTable<Owner>("github_repository_owners", (data) => {
+                if (!data) return;
+                owners = data;
+            });
+        }
+
+        if (!publicRepositories) {
+            res.sendStatus(403);
+        }
+
+        res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
+        res.setHeader("Content-Type", "application/json");
+
+        const completedRepositories = publicRepositories!.map((repo) => {
+            const login = repo.owner;
+            const owner = owners.find((owner) => owner.login === login) || {
+                avatar_url:
+                    "https://avatars.githubusercontent.com/u/9919?s=200",
+                html_url: "https://github.com/",
+                login: "???",
+                type: "User",
+            };
+            return { ...repo, owner } as Repository;
+        });
+
+        res.send(JSON.stringify(completedRepositories));
+    });
 };
--- backend/src/helpers/external.ts
+++ backend/src/helpers/external.ts
@@ -9,6 +9,52 @@ import { createClient } from "@supabase/supabase-js";
 let client: ReturnType<typeof createClient> | undefined = undefined;
 
 /**
+ * Names of different tables
+ */
+export type Table =
+    | "github_last_update"
+    | "github_repository"
+    | "github_repository_owners";
+
+/**
+ * Properties of the owners table
+ */
+export type Owner = {
+    login: string;
+    avatar_url: string;
+    html_url: string;
+    type: "User" | "Organization" | "Bot" | "Mannequin";
+};
+
+/**
+ * Types of repository visiblity
+ */
+export type Visibility = "public" | "private";
+
+/**
+ * Properties of the repository table
+ */
+export type SimplifiedRepository = {
+    name: string;
+    html_url: string;
+    owner: string;
+    visibility: Visibility;
+    description?: string;
+    fork: boolean;
+    archived: boolean;
+    languages_url: string;
+};
+
+/**
+ * Method that throws a `ReferenceError` if the client wasn't initialized yet
+ */
+const checkClient = () => {
+    if (!client) {
+        throw new ReferenceError("Client has not been initialized.");
+    }
+};
+
+/**
  * Creates a Supabase instance, if not already created.
  * @returns New Supabase instance, the previously created one, or `undefined`.
  */
@@ -18,8 +64,48 @@ export const createSupabase = () => {
     }
 
     if (canConnectToDatabase()) {
-        client = createClient(DATABASE_URL!, DATABASE_KEY!);
+        try {
+            console.log(`⛃ Creating Supabase instance...`);
+            client = createClient(DATABASE_URL!, DATABASE_KEY!);
+            console.log(`⛃✅ Supabase instance created!`);
+        } catch (error) {
+            console.error(`⛃❌ Could not create Supabase instance:`, error);
+        }
     }
 
     return client;
+};
+
+/**
+ * Get all rows from a table
+ * @param name Name of the table
+ * @param callback Callback to pass the data once received
+ * @return Promise for database query, or resolved promise if an error occurs before the query starts
+ */
+export const getTable = <T>(
+    name: Table,
+    callback: (data: T[] | null) => void,
+) => {
+    console.log(`⛃ Querying table [${name}]...`);
+    try {
+        checkClient();
+
+        return client!
+            .from(name)
+            .select("*")
+            .then(({ data, error }) => {
+                if (error) {
+                    console.error(
+                        `⛃❌ Query to table [${name}] failed:`,
+                        error,
+                    );
+                    return callback(null);
+                }
+                callback(data as T[] | null);
+                console.log(`⛃✅ Queried table [${name}] and ran callback.`);
+            });
+    } catch (error) {
+        console.error(`⛃❌ Could not create query to table [${name}]:`, error);
+        return Promise.resolve();
+    }
 };
