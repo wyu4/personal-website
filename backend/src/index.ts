f0e323d14ab42d5c982f565abda62d6ba950d0f8
wyu4 <wilsonyu657@gmail.com>
2026-03-30 23:27:56 -0400

    Create enpoints before listening to port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,11 +20,11 @@ app.use(
 app.use(express.static(path.join(__dirname, "src")));
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("✅ Express running on http://localhost:3000");
-});
-
 console.log("☁️ Creating endpoints...");
 createRootAPI(app);
 createRepositoriesAPI(app);
 console.log("☁️✅ Endpoints created!");
+
+app.listen(3000, () => {
+    console.log("✅ Express running on http://localhost:3000");
+});
